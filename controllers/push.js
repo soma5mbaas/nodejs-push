4d6048da1a035c0b0c14e3fe27f5df6bf7dd66f6
syntaxfish <syntaxfish@gmail.com>
2014-10-06 18:25:12 +0900

    send json 변경

--- controllers/push.js
+++ controllers/push.js
@@ -5,13 +5,14 @@ var getHeader = require('haru-nodejs-util').common.getHeader;
 exports.push = function(req, res) {
 	var options = getHeader(req);
 	var notification = req.body.notification || {};
+	var output = {};
 
 	options.channels = req.body.channels || [];
 	options.where = req.body.where || {};
 
 	pushHandelr.pushNotification(options, notification, function(error, results) {
 		if( error ) return res.json(error, results);
-
-		res.json(results);
+		
+		res.json(output);
 	});
 };
