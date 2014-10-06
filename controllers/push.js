54d02ea7c7a7e9b190ea6c3648a2dd1e0795f68b
syntaxfish <syntaxfish@gmail.com>
2014-10-06 18:02:08 +0900

    pushManager.pushNotification 함수 인자값 변경

--- controllers/push.js
+++ controllers/push.js
@@ -4,12 +4,12 @@ var getHeader = require('haru-nodejs-util').common.getHeader;
 
 exports.push = function(req, res) {
 	var options = getHeader(req);
-	var data = req.body.data || {};
+	var notification = req.body.notification || {};
 
 	options.channels = req.body.channels || [];
 	options.where = req.body.where || {};
 
-	pushHandelr.pushNotification(options, data, function(error, results) {
+	pushHandelr.pushNotification(options, notification, function(error, results) {
 		if( error ) return res.json(error, results);
 
 		res.json(results);
