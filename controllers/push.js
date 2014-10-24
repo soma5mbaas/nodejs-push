a21598f563ce946907d4c83ddd8e1dfafdfcafd8
syntaxfish <syntaxfish@gmail.com>
2014-10-24 18:03:57 +0900

    push user query 추가

--- controllers/push.js
+++ controllers/push.js
@@ -1,6 +1,8 @@
 
 var pushHandelr = require('../handlers/push');
 var getHeader = require('haru-nodejs-util').common.getHeader;
+var sendError = require('haru-nodejs-util').common.sendError;
+
 
 exports.push = function(req, res) {
 	var options = getHeader(req);
@@ -8,11 +10,13 @@ exports.push = function(req, res) {
 	var output = {};
 
 	options.channels = req.body.channels || [];
-	options.where = req.body.where || {};
+	options.where = { installations: req.body.installations || {}, users: req.body.users };
+
+	if( !req.body.installations && !req.body.users ) { return sendError(res, new Error('')); }
 
 	pushHandelr.pushNotification(options, notification, function(error, results) {
-		if( error ) return res.json(error, results);
+		if( error ) { return sendError(res, error); }
 		
-		res.json(output);
+		res.json({total: results});
 	});
 };
--- handlers/push.js
+++ handlers/push.js
@@ -1,67 +1,83 @@
-
-var mongodb = require('../connectors/mongodb');
-
 var async = require('async');
 
 var keys = require('haru-nodejs-util').keys;
-var isEmptyObject = require('haru-nodejs-util').common.isEmptyObject;
+var store = require('haru-nodejs-store');
 
 var PushManager = require('../utils/pushManager');
 
 var pushManger = new PushManager({});
 
 var InstallationsClass = 'Installations';
+var UsersClass = 'Users';
+
 
 exports.pushNotification = function(options, notification, callback) {
-
-	var done = function( error, results ) {
-		return callback(error, results);
-	};
-
-	async.waterfall([
-		function findDeviceTokens(callback) {
-			var key = keys.collectionKey(InstallationsClass, options.applicationId);
-			function returenDeviceToken(error, results) {
-				var providers = {
-					gcm: [],
-					ios: [],
-					mqtt: []
-				};
-
-				results.forEach(function(installation) {
-					var type = installation.pushType;
-                    if( providers[type] ) {
-                        providers[type].push(installation.deviceToken);
-                    } else if( installation.deviceType === 'android' ) {
-                        providers.gcm.push(installation.deviceToken);
-                    } else if( installation.deviceType === 'ios' ) {
-                        providers.apnspush(installation.deviceToken);
-                    }
+    async.waterfall([
+        function queryUsers(callback){
+			if( options.where.users ) {
+				store.get('mongodb').find( keys.collectionKey(UsersClass, options.applicationId) , options.where.users, function(error, results) {
+					callback(null , results);
 				});
+			} else {
+				callback(null , null);
+			}
+        },
+		function queryInstallations(userList, callback) {
+			if(userList) {
+				var userIds = [];
+				for(var i = 0; i < userList.length; i++) {
+					userIds.push(userList[i]._id);
+				}
+				options.where.installations['userId'] = {$in: userIds};
+			}
 
-				return callback( error, providers );
+			store.get('mongodb').find( keys.collectionKey(InstallationsClass, options.applicationId) , options.where.installations, function(error, results) {
+				callback(null , results);
+			});
+		},
+        function sendPush(installations, callback) {
+			var providers = {
+				gcm: [],
+				ios: [],
+				mqtt: []
 			};
 
+			for(var i = 0; i < installations.length; i++) {
+				var installation = installations[i];
+				var type = installation.pushType;
 
-			if( !isEmptyObject(options.where) ) {
-				mongodb.find(key, options.where, returenDeviceToken);
-    
-			} else if( !isEmptyObject(options.channels) ) {
-				var condition = { channels: {"$in": options.channels} };
-				mongodb.find(key, condition, returenDeviceToken);
+				if( providers[type] ) {
+					providers[type].push(installation.deviceToken);
+				} else if( installation.deviceType === 'android' ) {
+					providers.gcm.push(installation.deviceToken);
+				} else if( installation.deviceType === 'ios' ) {
+					providers.apnspush(installation.deviceToken);
+ 				}
 			}
-		},
-		function push(providers, callback) {
-			var keys = Object.keys(providers);
-            keys.forEach(function(pushType) {
+
+			var providerKeys = Object.keys(providers);
+
+			async.times(providerKeys.length, function(n, next) {
+			    var pushType = providerKeys[n];
+
 				if( providers[pushType].length > 0){
 					pushManger.notify( options.applicationId, pushType, notification, providers[pushType], function() {
-
+						next( null,  providers[pushType].length);
 					});
+				} else {
+					next(null, 0);
 				}
+			},function done(error, results) {
+				callback(null, results);
 			});
 
-			callback(null, null);
+        }
+    ], function done(error, results) {
+		var total = 0;
+		for( var i = 0; i < results.length; i++) {
+			total += results[i];
 		}
-		], done);
+
+        callback(error, total);
+    });
 };
