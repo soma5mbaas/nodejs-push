1b4f8773f2e94ebaab79f3054c18a2ed1816fe0a
syntaxfish <syntaxfish@gmail.com>
2014-12-23 02:06:34 +0900

    remove routers

--- routes/push.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var push = require('../controllers/push');
-
-/* GET home page. */
-router.post('/', push.push);
-
-module.exports = router;
